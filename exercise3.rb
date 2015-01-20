3a49210663e997e41b52c2e4cb9e102474279c57
lauranjansen <lauran.jansen@gmail.com>
2015-01-20 15:35:54 -0500

    Add year of birth calculation

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
+require 'date'
 puts "What is your name?"
-name = gets.chomp
-puts "Hi #{name}!"
+name = gets.chomp #ask for name
+puts "Hi #{name}!" #output name
+puts "What is your age?"
+age = gets.chomp.to_i #ask for age and convert
+year = Date.today.year
+puts "You're #{age} years old, which means you were born in #{year-age} or #{year-1-age}."
